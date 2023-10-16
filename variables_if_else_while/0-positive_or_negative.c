69659b55c5cfdfde92e567e2d63193f79813d3e6
aaronlopez1498 <lopez.aaron179@gmai.com>
2023-10-16 11:46:34 -0400

    fixing

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -5,12 +5,12 @@
  * main - program to sort numbers if positive negative or zero
  * Return positive if >0, negative if <0, zero if =0
  */
-in 'main'(void)
+in main(void)
 {
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	n = rand() -RAND_MAX/2;
 	if (n > 0)
 {
 	printf("%d is positive\n", n);
