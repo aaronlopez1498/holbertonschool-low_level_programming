e5f9325142023eef85490aa3e8df70f4b94f8ef7
aaronlopez1498 <lopez.aaron179@gmai.com>
2023-10-17 13:44:24 -0400

    putcher

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * main - Prints "Holberton" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putcher('H');
+	_putcher('o');
+	_putcher('l');
+	_putcher('b');
+	_putcher('e');
+	_putcher('r');
+	_putcher('t');
+	_putcher('o');
+	_putcher('n');
+	_putcher('\n');
+
+	return (0);
+}
