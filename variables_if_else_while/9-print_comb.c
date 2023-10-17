5469ed6904a3cbf481baa66ed912f092914f5aa9
aaronlopez1498 <lopez.aaron179@gmai.com>
2023-10-17 12:53:20 -0400

    fixing 9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -4,17 +4,17 @@
  *
  * Return: Always 0.
  */
-int main(voi)
+int main(void)
 {
 
 	int num;
 
 	for (num = 0; num <= 9; num++)
 	{
-		putchar((num % 10) + '0'); if (num == 9);
-		continue;
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
 		putchar(',');
-
 		putchar(' ');
 
 	}
