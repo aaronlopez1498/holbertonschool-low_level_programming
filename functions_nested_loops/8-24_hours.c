2b6e1b2991eb8c4692e5787937273e1b2e2b9e1b
aaronlopez1498 <lopez.aaron179@gmai.com>
2023-10-19 14:03:37 -0400

    24hrs

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print every minute of the day of jack Bauer, starting from 00:00 to 23:59.
+ *
+ * Return: Always 0
+ */
+int void jack_bauer(void);
+{
+	jack_bauer();
+	if (head is 00:00)
+		_putchar 00:00
+
+			last(tail is 23:59)
+			_putchar 23:59
+
+			return (0);
+}
+
