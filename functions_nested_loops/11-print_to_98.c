752b3f9bb23b379dc17e682610e03f8b5be41a0f
aaronlopez1498 <lopez.aaron179@gmai.com>
2023-10-20 09:25:14 -0400

    prtint

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,4 @@
-include <stdio.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -10,7 +10,7 @@ void print_to_98(int n)
 	if (n >= 98)
 	{
 		while (n > 98)
-			prinft"(%d, ", n--);
+			prinft("%d, ", n--);
 		printf("%d\n", n);
 	}
 	else
