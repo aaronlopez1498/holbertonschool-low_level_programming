9ed731dd418da2e20071d4ab56751c8f26adb084
aaronlopez1498 <lopez.aaron179@gmai.com>
2023-10-20 01:28:39 -0400

    9 time

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -4,9 +4,10 @@
 /**
  * times_table - Prints the 9 times table, startimg with 0.
  */
-void time_table(void)
+void times_table(void)
 {
 	int num, mult, prod;
+	time _table();
 
 	for (num = 0; num <= 9; num++)
 	{
