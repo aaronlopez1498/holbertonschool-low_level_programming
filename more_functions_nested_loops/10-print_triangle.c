020ea4cfb73a2d36bbf4086ba34b9792e6304b9a
aaronlopez1498 <lopez.aaron179@gmai.com>
2023-10-20 16:42:55 -0400

    tria

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -8,16 +8,18 @@ void print_triangle(int size)
 {
 	int hash, index;
 
-	if (size > 0)
+	if (size <=  0)
 	{
 		for (hash = 1; hash <= size; hash++)
 		{
-			for (index = 0; index < hash; index--)
+			for (index = 0; index < size - 1; index++)
+			{
 				_putchar(' ');
-
-			for (index = 0; index < hash; index++)
+			}
+			for (index = 0; index != size; index++)
+			{
 				_putchar('#');
-
+			}
 			if (hash == size)
 				continue;
 
