ca3e16993e80ad6c35f1f38a68911ab19cdeaf9c
aaronlopez1498 <lopez.aaron179@gmai.com>
2023-10-23 13:29:43 -0400

    swapfinis

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,8 +1,14 @@
 #include "main.h"
-
+#inlude <stdio.h>
+/**
+ * swap_int- Execute function
+ * @a:pointer that points to an int
+ * @b: pointer that point to an int
+ */
 void swap_int(int *a, int *b)
 {
-	int tmp = *a;
+	int prov;
+   prov = *a;
 	*a = *b;
-	*b = tmp;
+	*b = prov;
 }
