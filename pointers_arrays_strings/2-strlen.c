ff8ce29ac3d2f3aba825c5984e56dcb5d609280b
aaronlopez1498 <lopez.aaron179@gmai.com>
2023-10-23 13:51:46 -0400

    str2f

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -4,7 +4,7 @@
  *_strlen - Return the length of a string.
  * @str: The string to get the length of.
  *
- * Return: The length of @str
+ * Return: The length of str
  */
 
 size_t _strlen(const char *str)
