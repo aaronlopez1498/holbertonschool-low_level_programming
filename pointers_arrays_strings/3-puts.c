b74fb941deaa6f289167eda743daaf8450e4b97d
aaronlopez1498 <lopez.aaron179@gmai.com>
2023-10-23 14:31:11 -0400

    putsf

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,13 +1,17 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * _puts - prints a string to stdout.
- * @str: The string to be printed.
+ * _puts- Execute function
+ * @str: holds astring
+ *
  */
 void _puts(char *str)
 {
-	while (*str)
-		_putchar(*str++);
+	int i;
 
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		_putchar(str[i]);
+	}
 	_putchar('\n');
 }
