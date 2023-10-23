1fe86809a9fa60b43b846d4c42c4bff10e4caa25
aaronlopez1498 <lopez.aaron179@gmai.com>
2023-10-23 14:45:16 -0400

    pts3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,17 +1,13 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * _puts- Execute function
- * @str: holds a string
- *
+ * _puts - Prints a string so stdout.
+ * @str: The string to be printed
  */
 void _puts(char *str)
 {
-	int i;
+	while (*str)
+		_putchar(*str++);
 
-	for (i = 0; str[i] != '\0'; ++i)
-	{
-		_putchar(str[i]);
-	}
 	_putchar('\n');
 }
