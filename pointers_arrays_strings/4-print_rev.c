bd17c27fe16f85ba260c206664884616d75e0fa7
aaronlopez1498 <lopez.aaron179@gmai.com>
2023-10-24 11:30:50 -0400

    pit

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -3,20 +3,20 @@
 /**
  * print_rev - Prints a string in reverse
  *
- * @n: the string to print
+ * @s: the string to print
  */
-void print_rev(char *n)
+void print_rev(char *s)
 
 {
 	int c = 0;
 
-	while (n[c] != '\0')
+	while (s[c] != '\0')
 	{
 		c++;
 	}
 	for (c -= 1; c >= 0; c--)
 	{
-		_putchar(n[c]);
+		_putchar(s[c]);
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
