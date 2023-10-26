ea19b7527c50f9091c9a0ca189043d8d24900313
aaronlopez1498 <lopez.aaron179@gmai.com>
2023-10-26 11:00:43 -0400

    str2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -10,6 +10,7 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int size = 0, j = 0;
+
 	while (dest[size])
 	{
 		size++;
@@ -17,7 +18,7 @@ char *_strncpy(char *dest, char *src, int n)
 	for (j = 0; j < n && src[j] != '\0'; j++)
 		dest[j] = src[j];
 
-	for (;j < n; j++)
+	for (; j < n; j++)
 		dest[j] = '\0';
 
 	return (dest);
