836691ce95c955ecd11444e75940256d6d3c6d56
aaronlopez1498 <lopez.aaron179@gmai.com>
2023-10-26 11:18:56 -0400

    array4

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reverse_array- Exevute function
+ * @a: points to an int array
+ * @n: hols an int (size)
+ */
+void reverse_array(int *a, int n)
+{
+	/*Variablesa*/
+	int f = 0, temp;
+	/*Reverse loop*/
+	for (; f < n--; f++)
+	{
+		/*switcht position*/
+		temp = a[n];
+		a[n] = a[f];
+		a[f] = temp;
+	} /*end for*/
+} /*end reverse_array*/
