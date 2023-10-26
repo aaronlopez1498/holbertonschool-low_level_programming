9ec43b36694e921694f13d4410bdbc105131db8a
aaronlopez1498 <lopez.aaron179@gmai.com>
2023-10-26 10:01:43 -0400

    srt0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,20 +1,25 @@
 #include "main.h"
 
+
 /**
- * @dest: A pointer to the string to be concatenated upon.
- * @src: The source string to be appended to @dest.
+ * _strcat - Execute function
+ * @dest: Points to a char
+ * @src: Points to a char
  *
- * Return: A pointer to the destination string @dest.
+ * Return: return dest pointer
  */
+char *_strcat(char *dest, const char *src)
+ {
+	 int size = 0, j;
 
- do{
-	int index = 0, dest_len = 0;
-
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-
-	return (dest);
-}
+	 while (dest[size])
+	 {
+		 size++;
+	 }
+	 for (j = 0; j < size + 1 && src[j]; j++)
+	 {
+		 dest[size++] = src[j];
+	 }
+	 dest[size] = '\0';
+	 return (dest);
+ }
