1544d75cd2e39d033f98eba3fd13756a412c0386
aaronlopez1498 <lopez.aaron179@gmai.com>
2023-10-26 11:53:54 -0400

    cap

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ **cap_string- execute cap function
+ **word_start- execute bool function
+ *@str: point to a char string
+ *Return: in cap the string, in bool the result
+ */
+char *cap_string(char *str)
+{
+	/*variables*/
+	int i = 0, j;
+	char Separator[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	/*loops for moving along the data in the arrays*/
+	for (; str[i] != '\0'; i++)
+	{
+		/*check if*/
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+			str[i] -=32
+				for (j = 0; j < 13 ; j++)
+				{
+					if (str[i] == Separator[j])
+					{
+						if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+						{
+							str[i +1] -=32;
+						}
+					} /*end if else*/
+				}
+	} /*end for*/
+	return (str);
+} /*end cap_string*/
