3cb41035e60207a47cc5e704bb2069dd5756ef91
aaronlopez1498 <lopez.aaron179@gmai.com>
2023-10-26 13:14:14 -0400

    cap

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,29 +1,32 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- **cap_string- execute cap
- **word_start- execute bool
- *@str: point to a char string
- *Return: in cap the string
- */
-char *cap_string(char *str)
+ 
+cap_string - Capitalize all words
+@s: the string
+Return: a pointer to change
+*/
+
+char cap_string(chars)
 {
-	int i = 0, j;
-	char Separator [13] = {' ', '\t', '\n', ',', ';', '.', '!', '?',
-	for (; str[i] != '\0'; i++)
-	{
-		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
-			str[i] -= 32;
-		for (j = 0; j < 13 ; j++)
-		{
-			if (str[i] == Separator[j])
-			{
-				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-				{
-					str[i + 1] -= 32;
-				}
-			}
-		}
-	}
-	return (str);
+    int count = 0, i;
+    int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+    if ((s + count) >= 97 &&(s + count) <= 122)
+        (s + count) =(s + count) - 32;
+    count++;
+    while ((s + count) != '\0')
+    {
+        for (i = 0; i < 13; i++)
+        {
+            if ((s + count) == sep_words[i])
+            {
+                if (((s + (count + 1)) >= 97) && ((s + (count + 1)) <= 122))
+                    (s + (count + 1)) =(s + (count + 1)) - 32;
+                break;
+            }
+        }
+        count++;
+    }
+    return (s);
 }
