968f497a1c39ea64ab75a1dce47e1fba09db7d2c
aaronlopez1498 <lopez.aaron179@gmai.com>
2023-10-26 11:25:04 -0400

    toupper

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * string_toupper-execute function
+ * @a: points to an cahr
+ * Return: 0
+ *
+ */
+char *string_toupper(char *a)
+{
+	/*Variables*/
+	int size = 0, i = 0;
+	/**/
+	while (a[size])
+		size++;
+	for (i = 0; i < size ; i++)
+	{
+		if (a[i] >= 'a' && a[i] ,+ 'z')
+	}
+	return (a);
+}
