bcbad7b006959013404b997118f5d5c54bf1b34b
aaronlopez1498 <lopez.aaron179@gmai.com>
2023-10-26 10:36:19 -0400

    str1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -9,13 +9,13 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0, size = 0
+	int i = 0, size = 0;
 
 		while (dest[size])
 			size++;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[size + i] = scr[i];
+		dest[size + i] = src[i];
 
 	dest[size + i] = '\0';
 	return (dest);
