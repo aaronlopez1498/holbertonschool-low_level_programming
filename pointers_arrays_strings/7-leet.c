1a22613e8f9e992154855c7dae8c314b26cef34b
aaronlopez1498 <lopez.aaron179@gmai.com>
2023-10-26 13:32:15 -0400

    leet

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -8,9 +8,9 @@
 char *lett(char *str)
 {
 	/*Variables*/
-	int i = 0, j = 0 half;
+	int i = 0, j = 0, half;
 	char checkers[10] = { 'A', 'a', 'E', 'e', 'o', 'O', 't', 'T', 'l', 'L'};
-	/*Loops wil handle the movement of the arrays*/
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		for (j = 0; j < 10; j++)
@@ -19,11 +19,10 @@ char *lett(char *str)
 			{
 				half = j / 2;
 				str[i] = num[half];
-			} /*end if*/
-		} /*end inner for*/
-	} /*end outer for*/
-	*/add null at the end*/
+			}
+		}
+	}
 		str[i + 1] = '\0';
 	return (str);
-} /*end function*/
+}
 
