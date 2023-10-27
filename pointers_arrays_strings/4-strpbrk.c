4ecd4ed14b277a94d464dabd690d21a4c7585626
aaronlopez1498 <lopez.aaron179@gmai.com>
2023-10-27 14:24:55 -0400

    str4

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *_strpbrk - Searches a string for any of a set of byte.
+ *@s: The string to be searched.
+ *@accept: the setv of bytes to be searched for.
+ *
+ *Return: ifa set is matchad - a pointer to the matched byte.
+ if no set is matched - NULL
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int index;
+
+	while (*s)
+	{
+		for (index = 0; accept[index]; index++)
+		{
+			if (*s ==accept[index])
+				return (s);
+		}
+		s++;
+	}
+	return ('\0');
+}
