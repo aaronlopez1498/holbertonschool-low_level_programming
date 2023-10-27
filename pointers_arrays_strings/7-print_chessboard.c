196a074be5eb21d445fa1204d7b803920c932298
aaronlopez1498 <lopez.aaron179@gmai.com>
2023-10-27 15:24:15 -0400

    print

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,18 +1,38 @@
+#include <stdio.h>
 #include "main.h"
-
 /**
- * print_chessboard - print a chessboard.
- * @a: the chessboard to be printed
- */
-void print_chessboard(char (*a)[8])
+ *print_chessboard- executes funtion
+ *@a: points to a 2 dimentional array
+ *
+ /
+void print_chessboard(char (a)[8])
 {
-	int indx1, indx2;
+    int i = 0, j = 0;
+    char pices[8] = {'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'};
+    char pawns[8] = {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'};
 
-	for (indx1 = 0; a[indx1 < 7]; indx2++)
-	{
-		for (indx2 = 0; indx2 < 8; indx2++)
-			_putchar(a[indx1][indx2]);
-
-		_putchar('\n');
-	}
-}
+    for (i = 0; i < 8; i++)
+    {
+        for (j = 0; j < 8; j++)
+        {
+            switch (i)
+            {
+                case 0:
+                    a[i][j] = pices[j];
+                    break;
+                case 1:
+                    a[i][j] = pawns[j];
+                    break;
+                case 6:
+                    a[i][j] = pawns[j] - 32;
+                    break;
+                case 7:
+                    a[i][j] = pices[j] - 32;
+                    break;
+            } /end switch/
+            _putchar(''');
+            _putchar(a[i][j]);
+            _putchar
+        } /end for/
+    } /end outer for/
+} /end function/
