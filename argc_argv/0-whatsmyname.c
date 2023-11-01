ac0c7b4ef67dedece3067547d11a930cf1bd91ca
aaronlopez1498 <lopez.aaron179@gmai.com>
2023-11-01 13:12:13 -0400

    myname

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints the program name, followed by a new line.
- * @argc: the number of arguments supplied to the program.
- * @argv: an array of pointers to the arguments.
+ * main - Prints the program name, followed by a new line.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
  * Return: Always 0.
  */
