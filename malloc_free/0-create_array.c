cb519d7a3b9948e26c9f05d039ad6c09d988aefb
aaronlopez1498 <lopez.aaron179@gmai.com>
2023-11-07 10:36:58 -0400

    all

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -12,6 +12,7 @@
 	*          Otherwise - a pointer to the array.
 	*/
 char *create_array(unsigned int size, char c);
+
 {
 	char *array;
 	unsigned int index;
@@ -20,9 +21,11 @@ char *create_array(unsigned int size, char c);
 		return (NULL);
 
 	array = malloc(sizeof(char) * size);
-	if (array == NULL);
 
-	for (index = 0; index < size : index++)
+	if (array == NULL);
+	return(NULL);
+
+	for (index = 0; index < size; index++)
 		array[index] = c;
 
 	return (array);
