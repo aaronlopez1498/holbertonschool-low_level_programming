56484e6eb32653e5421b29efaf0eeb0830df1ab6
aaronlopez1498 <lopez.aaron179@gmai.com>
2023-11-07 10:31:41 -0400

    all

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -19,12 +19,11 @@ char *create_array(unsigned int size, char c);
 	if (size == 0)
 		return (NULL);
 
-	array = malloc (sizeof(char) * size);
-
+	array = malloc(sizeof(char) * size);
 	if (array == NULL);
 
-	for (index = 0; index < size: index++)
-		array[index] =c;
+	for (index = 0; index < size : index++)
+		array[index] = c;
 
-	return(array);
+	return (array);
 }
